9acd32fa1b2d2605d967519081e0d4782228b4ea
jkodu <jey.nandakumar@gmail.com>
2019-11-04 14:21:27 +0000

    change layout component to functional component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,104 +1,63 @@
-import React from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql, Link } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import ReactMedia from 'react-media'
-
 import Navigation from '../navigation'
 import Footer from '../footer'
 
 import 'normalize.css'
 import './index.scss'
 
-class Layout extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			showMenu: true,
-		}
-	}
+const Layout = ({ children, location }) => {
+	const [showMenu, setShowMenu] = useState(true)
 
-	componentDidMount() {
-		const { hash } = window.location
-		if (!hash) {
+	const data = useStaticQuery(
+		graphql`
+			query {
+				getSiteTitle: site {
+					siteMetadata {
+						actRulesPackage
+					}
+				}
+			}
+		`
+	)
+
+	useLayoutEffect(() => {
+		/**
+		 * Scroll to anchor if specified
+		 */
+		if (!location || !location.hash) {
 			return
 		}
-		const el = document.getElementById(hash)
+		const el = document.querySelector(location.hash)
 		if (!el) {
 			return
 		}
 		el.scrollIntoView()
-	}
+	})
 
-	handleHideShowMenu() {
-		this.setState(prevState => ({
-			showMenu: !prevState.showMenu,
-		}))
-	}
+	const { getSiteTitle } = data
+	const { author, repository } = JSON.parse(getSiteTitle.siteMetadata.actRulesPackage)
+	const { url: actRulesRepoUrl } = repository
 
-	getListItemFromEdges(edges) {
-		return edges.map(edge => {
-			const { node } = edge
-			const { path, context } = node
+	const handleHideShowMenu = () => setShowMenu(!showMenu)
 
-			if (!context || !context.title) {
-				return null
-			}
-
-			const key = `${context.title}${path}`
-			return (
-				<li key={key}>
-					<Link activeClassName="active" to={path}>
-						{context.title}
-					</Link>
-				</li>
-			)
-		})
-	}
-
-	render() {
-		const { children } = this.props
-		return (
-			<StaticQuery
-				query={graphql`
-					query {
-						getSiteTitle: site {
-							siteMetadata {
-								actRulesPackage
-							}
-						}
-					}
-				`}
-				render={data => {
-					const { getSiteTitle } = data
-					const {
-						author,
-						repository: { url: actRulesRepoUrl },
-					} = JSON.parse(getSiteTitle.siteMetadata.actRulesPackage)
-					return (
-						<section className="layout-container">
-							{/* hide menu when width <= 600px */}
-							<ReactMedia
-								query="(max-width: 600px)"
-								onChange={matches => matches && this.state.showMenu && this.handleHideShowMenu()}
-							/>
-							{/* show menu when width > 600px */}
-							<ReactMedia
-								query="(min-width: 601px)"
-								onChange={matches => matches && !this.state.showMenu && this.handleHideShowMenu()}
-							/>
-							{/* side bar navigation  */}
-							<Navigation logoName={author.name} logoNavigateTo={'/pages/about'} />
-							{/* main content  */}
-							<main>
-								<section>{children}</section>
-								<Footer actRulesRepoUrl={actRulesRepoUrl} />
-							</main>
-						</section>
-					)
-				}}
-			/>
-		)
-	}
+	return (
+		<section className="layout-container">
+			{/* hide menu when width <= 600px */}
+			<ReactMedia query="(max-width: 600px)" onChange={matches => matches && showMenu && handleHideShowMenu()} />
+			{/* show menu when width > 600px */}
+			<ReactMedia query="(min-width: 601px)" onChange={matches => matches && !showMenu && handleHideShowMenu()} />
+			{/* side bar navigation  */}
+			<Navigation logoName={author.name} logoNavigateTo={'/pages/about'} />
+			{/* main content  */}
+			<main>
+				<section>{children}</section>
+				<Footer actRulesRepoUrl={actRulesRepoUrl} />
+			</main>
+		</section>
+	)
 }
 
 Layout.propTypes = {
