3b5a65e31d61a6a1a3d35352d8e67d65a97d7b7a
Jean-Yves Moyen <jym@siteimprove.com>
2019-11-27 13:58:18 +0100

    Update from review

--- src/components/accessibility_requirements/index.js
+++ src/components/accessibility_requirements/index.js
@@ -111,7 +111,7 @@ function TechniqueListing({ item, mapping, listType }) {
 			listType={listType}
 			title={title}
 			learnMore={title}
-			conformanceTo="Not required to conform to any known conformance document."
+			conformanceTo="Not required to conformance to any W3C accessibility recommendation."
 			url={url}
 			mapping={mapping}
 		/>
