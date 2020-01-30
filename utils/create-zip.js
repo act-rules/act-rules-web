be13ea470f4ebe2c3309b92e9947f46303ce8fbe
jkodu <jey.nandakumar@gmail.com>
2020-01-30 12:13:12 +0000

    remove log

--- utils/create-zip.js
+++ utils/create-zip.js
@@ -1,13 +1,11 @@
 const zipFolder = require('zip-folder')
 
 const createZip = (folder, name) => {
-	console.log(folder)
 	return new Promise((resolve, reject) => {
 		zipFolder(folder, name, err => {
 			if (err) {
 				reject(err)
 			}
-			console.log('111')
 			resolve()
 		})
 	})
