71eb959a8fce2dc0bc66197492e5701936a98cba
jkodu <jey.nandakumar@gmail.com>
2020-01-31 11:56:13 +0000

    chore: do not zip until large file pushing is sorted

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,7 +10,7 @@ exports.onPreBootstrap = async () => {
 	 * `gatsby build` cleans all `html` and `css` files within the destination directory, hence the need to copy these during `bootstrap` step
 	 */
 	await copy('./_data/testcases', 'public')
-	await createZip('./_data/testcases', 'public/testcases.zip')
+	// await createZip('./_data/testcases', 'public/testcases.zip')
 
 	/**
 	 * copy `earl-context.json` so it can be available in URL - `https://act-rules.github.io/earl-context.json`
