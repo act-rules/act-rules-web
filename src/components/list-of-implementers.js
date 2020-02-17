01eb4016ac67e214a67368463fb95013dade8a31
Jey Nandakumar <jey.nandakumar@gmail.com>
2020-02-17 09:52:39 +0000

    update list of implementations with more data (#131)

--- src/components/list-of-implementers.js
+++ src/components/list-of-implementers.js
@@ -5,10 +5,40 @@ import getHyphenatedString from '../../utils/get-hyphenated-string'
 
 import Note from './note'
 
-const ListOfImplementers = ({ implementers = [], cls = ``, ruleId = `` }) => {
+const ListOfImplementers = ({ implementers = [], cls = ``, ruleId }) => {
 	if (!implementers.length) {
 		return (
 			<Note cls={``} title={`No Implementations`} body={`Implementation reports are not provided for this rule.`} />
+		)
+	}
+
+	if (!ruleId) {
+		return (
+			<table className={cls}>
+				<thead>
+					<tr>
+						<th>Organisation</th>
+						<th>Tool</th>
+						<th>Report</th>
+					</tr>
+				</thead>
+				<tbody>
+					{implementers
+						.sort((a, b) => a.organisation.localeCompare(b.organisation))
+						.map(({ organisation, toolName }) => {
+							const reportUrl = `/implementation/${getHyphenatedString(toolName)}`
+							return (
+								<tr key={toolName}>
+									<td>{organisation}</td>
+									<td>{toolName}</td>
+									<td>
+										<a href={reportUrl}>View Report</a>
+									</td>
+								</tr>
+							)
+						})}
+				</tbody>
+			</table>
 		)
 	}
 
@@ -16,25 +46,29 @@ const ListOfImplementers = ({ implementers = [], cls = ``, ruleId = `` }) => {
 		<table className={cls}>
 			<thead>
 				<tr>
-					<th>Organisation</th>
-					<th>Tool Name</th>
+					<th>Tool</th>
+					<th>Consitency</th>
+					<th>Complete</th>
 					<th>Report</th>
 				</tr>
 			</thead>
 			<tbody>
-				{implementers.map(({ organisation, toolName }) => {
-					const filename = getHyphenatedString(toolName)
-					const reportUrl = ruleId ? `/implementation/${filename}#${ruleId}` : `/implementation/${filename}`
-					return (
-						<tr key={toolName}>
-							<td>{toolName}</td>
-							<td>{organisation}</td>
-							<td>
-								<a href={reportUrl}>View Report</a>
-							</td>
-						</tr>
-					)
-				})}
+				{implementers
+					.sort((a, b) => a.organisation.localeCompare(b.organisation))
+					.map(({ toolName, actMapping }) => {
+						const reportUrl = `/implementation/${getHyphenatedString(toolName)}#id-${ruleId}`
+						const impl = actMapping.find(impl => impl.ruleId === ruleId)
+						return (
+							<tr key={toolName}>
+								<td>{toolName}</td>
+								<td className="capitalize">{impl.consistency}</td>
+								<td>{impl.complete ? `Yes` : `No`}</td>
+								<td>
+									<a href={reportUrl}>View Report</a>
+								</td>
+							</tr>
+						)
+					})}
 			</tbody>
 		</table>
 	)
