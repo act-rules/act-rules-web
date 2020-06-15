bc4ab1cf5860f654365c0b155f686b6ff8a06fc9
Jey Nandakumar <jey.nandakumar@gmail.com>
2020-06-15 07:19:18 -0400

    fix: update styles and content of 404 page (#186)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,21 @@
 import React from 'react'
-
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const NotFoundPage = ({ location }) => (
 	<Layout location={location}>
 		<SEO title="404: Not found" />
-		<h1>NOT FOUND</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<section className="page-default">
+			<h1>Page Not Found</h1>
+			<p>
+				The page you are looking for does not exist.
+				<br />
+				<Link to="/" title="go back to the home page">
+					Go back to the home page
+				</Link>
+			</p>
+		</section>
 	</Layout>
 )
 
